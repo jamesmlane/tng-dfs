8da6231a961bc48d88ac792533ec156d59e992e1
James Lane <jameslane93@gmail.com>
2023-06-13 22:14:19 -0400

    Improvements to merger trace plotter

--- src/tng_dfs/plot.py
+++ src/tng_dfs/plot.py
@@ -28,8 +28,8 @@ def plot_all_merger_traces(tree,mlpids,snap_threshold=20,
     '''plot_all_merger_traces:
 
     Args:
-        tree
-        mlpids
+        tree (Tree) - Tree object
+        mlpids 
         snap_threshold (int) - Threshold snapshot (default 20)
         mass_ratio_threshold (float) - Threshold mass ratio (default 0.1)
         mass_star_ratio_threshold (float) - Threshold stellar mass ratio
@@ -40,7 +40,7 @@ def plot_all_merger_traces(tree,mlpids,snap_threshold=20,
         axs (matplotlib.pyplot.axis) - axis object
     '''
     fontsize=10
-    mass_color = 'Black'
+    mass_color = 'Navy'
     mass_star_color = 'DarkRed'
 
     # Load the data
@@ -90,18 +90,18 @@ def plot_all_merger_traces(tree,mlpids,snap_threshold=20,
     # Plot the mass of the primary branch and show where mass increasing
     axs[2].plot(np.log10(mass[tree.main_branch_mask]),
         tree.main_branch_snap, linewidth=4., linestyle='solid', 
-        color=mass_color)
+        color=mass_color, label='DM')
     axs[2].plot(np.log10(
             mass[tree.main_branch_mask][tree.main_branch_mass_grow_mask]),
         tree.main_branch_snap[tree.main_branch_mass_grow_mask], linewidth=2., 
-        linestyle='solid', color='DodgerBlue')
+        linestyle='solid', color='DodgerBlue', label='DM growing')
     axs[2].plot(np.log10(mass_star[tree.main_branch_mask]),
         tree.main_branch_snap, linewidth=4., linestyle='solid', 
-        color=mass_star_color)
+        color=mass_star_color, label='Stars')
     axs[2].plot(np.log10(
             mass_star[tree.main_branch_mask][tree.main_branch_mass_grow_mask]),
         tree.main_branch_snap[tree.main_branch_mass_grow_mask], linewidth=2., 
-        linestyle='solid', color='DarkOrange')
+        linestyle='solid', color='DarkOrange', label='Stars growing')
 
     # Show shadow boxes across all axes where the mass grows
     main_branch_mass_grow_fill_snap_cents = (
@@ -169,17 +169,18 @@ def plot_all_merger_traces(tree,mlpids,snap_threshold=20,
         fontsize=fontsize)
 
     # Add exclusion rectangles
-    select_rec = mpl.patches.Rectangle((np.log10(mass_ratio_threshold),
-        snap_threshold),0-np.log10(mass_ratio_threshold),
-        100-snap_threshold, alpha=0.2, color='Grey', zorder=3)
-    axs[0].add_artist(select_rec)
-    select_stars_rec = mpl.patches.Rectangle((np.log10(mass_star_ratio_threshold),
-        snap_threshold),0-np.log10(mass_star_ratio_threshold),
-        100-snap_threshold, alpha=0.2, color='Grey', zorder=3)
-    axs[1].add_artist(select_stars_rec)
+    # select_rec = mpl.patches.Rectangle((np.log10(mass_ratio_threshold),
+    #     snap_threshold),0-np.log10(mass_ratio_threshold),
+    #     100-snap_threshold, alpha=0.2, color='Grey', zorder=3)
+    # axs[0].add_artist(select_rec)
+    # select_stars_rec = mpl.patches.Rectangle((np.log10(mass_star_ratio_threshold),
+    #     snap_threshold),0-np.log10(mass_star_ratio_threshold),
+    #     100-snap_threshold, alpha=0.2, color='Grey', zorder=3)
+    # axs[1].add_artist(select_stars_rec)
 
     # Add legend
     axs[0].legend(fontsize=12-len(mlpids)/2)
+    axs[2].legend(fontsize=8)
     # axs[1].plot(np.log10(mb_mass),mb_snap, color='DodgerBlue', 
     #             linewidth=3.)
     # axs[1].plot(np.log10(mb_mass)[where_mb_grow], mb_snap[where_mb_grow],
