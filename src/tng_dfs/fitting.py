9d6e17d42f72fb11bcbde8716e2400716f4ff642
James Lane <jameslane93@gmail.com>
2023-09-02 02:21:17 -0400

    Add likelihood for binned density

--- src/tng_dfs/fitting.py
+++ src/tng_dfs/fitting.py
@@ -111,6 +111,100 @@ def domain_prior_dens(densfunc, params):
         if A <= 0: return False
     return True
 
+# Binned density profiles
+
+def mloglike_binned_dens(*args,**kwargs):
+    '''loglike_binned_dens:
+    
+    Wrapper for the loglike_binned_dens function that returns the negative log 
+    likelihood        
+    '''
+    return -loglike_binned_dens(*args,**kwargs)
+
+def loglike_binned_dens(params, densfunc, r, rho, usr_log_prior=None, 
+    usr_log_prior_params=None, parts=False):
+    '''loglike_binned_dens:
+    
+    Log likelihood function for fitting a binned density profile
+    
+
+    Args:
+        params (list): List of parameters for the density profile
+        densfunc (function): Function that returns the density profile
+        r (array): Array of galactocentric spherical radii
+        rho (array): Array of binned density profile
+        usr_log_prior (function): Function that returns the log prior
+            supplied by the user. Call signature is
+            usr_log_prior(densfunc, params, *usr_log_prior_params)
+    
+    Returns:
+        loglike (float): Log likelihood of the density profile
+        and if parts=True:
+            logdiffsum (float): sum of the squared differences between the
+                density profile and the binned density profile
+            logprior (float): Log of the prior on the density profile
+            usrlogprior (float): Log of the user supplied prior
+    '''
+    # Evaluate the domain prior
+    if not domain_prior_binned_dens(densfunc, params):
+        return -np.inf
+    # Evaluate the prior on the density profile
+    logprior = logprior_binned_dens(densfunc, params)
+    # Evaluate any user supplied prior
+    if callable(usr_log_prior):
+        usrlogprior = usr_log_prior(densfunc, params, *usr_log_prior_params)
+        if np.isinf(usrlogprior):
+            return -np.inf
+    # Evaluate the density profile
+    logdens = np.log(densfunc(r, 0., 0., params))
+    # Evaluate the log likelihood
+    logdiffsum = np.sum(np.square(logdens-np.log(rho))) 
+    loglike = logdiffsum + logprior + usrlogprior
+    if parts:
+        return loglike, logdiffsum, logprior, usrlogprior
+    else:
+        return loglike
+
+def logprior_binned_dens(densfunc, params):
+    '''logprior_binned_dens:
+
+    Prior on the density profile
+
+    Args:
+        densfunc (function): Function that returns the density profile
+        params (list): List of parameters for the density profile
+    
+    Returns:
+        logprior (float): Log prior on the density profile
+    '''
+    if isinstance(densfunc, pdens.NFWProfile):
+        a,_ = params
+        # Place a log-uniform prior on a
+        a_min = 0.001
+        a_max = 1000.
+        prior_a = scipy.stats.loguniform.pdf(a, a_min, a_max)
+        return np.log(prior_a)
+    return 0
+
+def domain_prior_binned_dens(densfunc, params):
+    '''domain_prior_binned_dens:
+
+    Prior on the domain of the density profile
+
+    Args:
+        densfunc (function): Function that returns the density profile
+        params (list): List of parameters for the density profile
+    
+    Returns:
+        domain_prior (bool): True if the parameters are in the domain of the
+            density profile
+    '''
+    if isinstance(densfunc, pdens.NFWProfile):
+        a,A = params
+        if a <= 0: return False
+        if A <= 0: return False
+
+
 # action distribution functions
 
 def mloglike_fJ(*args,**kwargs):
